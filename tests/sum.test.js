23f55a72ebd267738f77ec3156fde435379b679d
knuthjertvik <knut.hjertvik@hafslundeco.no>
2024-05-31 11:59:05 +0200

    Revert "Introduction a breaking a change for rollback exercise"
    This reverts commit 983a5093751dd6f46df9c59b76d92d421a8f5b97.
    
    Revert commit

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -7,11 +7,3 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-function exampleFunction() {
-    console.log("This is a working function");
-}
-
-// Introducing a syntax error
-function brokenFunction() {
-    console.log("This function will break the application";
-}
